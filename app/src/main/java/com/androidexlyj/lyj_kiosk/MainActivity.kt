d6231793fb6cd65a6d48fb0d73afeb589baef3a5
yjlee9909 <yjlee9909@naver.com>
2023-05-06 15:44:40 +0900

    홈 아이콘 추가

--- app/src/main/java/com/androidexlyj/lyj_kiosk/MainActivity.kt
+++ app/src/main/java/com/androidexlyj/lyj_kiosk/MainActivity.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
-    lateinit var lyj_goHome : Button
+    lateinit var lyj_goHome : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         title = "LYJ KIOSK"
 
-        lyj_goHome = findViewById<Button>(R.id.lyj_goHome)
+        lyj_goHome = findViewById<ImageButton>(R.id.lyj_goHome)
         lyj_goHome.setOnClickListener {
             val intent = Intent(this, LoadingActivity::class.java)
             startActivity(intent)
