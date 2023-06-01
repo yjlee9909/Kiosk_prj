2ad844c56cc52fe9652400ab7fc6141475864280
yjlee9909 <yjlee9909@naver.com>
2023-06-01 23:02:59 +0900

    로딩 액티비티 코드 정렬

--- app/src/main/java/com/androidexlyj/lyj_kiosk/LoadingActivity.kt
+++ app/src/main/java/com/androidexlyj/lyj_kiosk/LoadingActivity.kt
@@ -21,7 +21,7 @@ class LoadingActivity : AppCompatActivity() {
         lyj_goOrder = findViewById<LinearLayout>(R.id.lyj_goOrder)
         imgView = findViewById<ImageView>(R.id.imageView)
 
-
+        // 메뉴 화면으로 이동
         lyj_goOrder.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
@@ -30,11 +30,13 @@ class LoadingActivity : AppCompatActivity() {
 
     // 백버튼 막기
     override fun onBackPressed() {
-//        super.onBackPressed()
+        // super.onBackPressed()
+        // 백 버튼 눌렸을 때 첫 번째 누른 시간 기록하고 2.5초 이내에 다시 안눌린다면 시간 갱신
         if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
             backKeyPressedTime = System.currentTimeMillis()
             return
         }
+        // 백 버튼 누른 이후 2.5초 이내에 다시 누르면 앱 종료
         if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
             // 종료
             finishAffinity()
