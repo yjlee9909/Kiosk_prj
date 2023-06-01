2cd8a1dfd56877a838d66c6ec0e7bf87cc50f7de
yjlee9909 <yjlee9909@naver.com>
2023-06-01 21:54:29 +0900

    ICE 음료 프래그먼트 코드 정리

--- app/src/main/java/com/androidexlyj/lyj_kiosk/iceCof_Fragment.kt
+++ app/src/main/java/com/androidexlyj/lyj_kiosk/iceCof_Fragment.kt
@@ -9,37 +9,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [iceCof_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class iceCof_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-
-
         val view = inflater.inflate(R.layout.fragment_icecof, container, false)
-
         val linearLayouts = listOf<LinearLayout>(
             view.findViewById(R.id.lyj_ice_ameri),
             view.findViewById(R.id.lyj_ice_latte),
@@ -57,8 +37,6 @@ class iceCof_Fragment : Fragment() {
                 handleLinearClick(linearLayout)
             }
         }
-
-
         return view
     }
 
@@ -96,26 +74,12 @@ class iceCof_Fragment : Fragment() {
             imageDrawable = imageView.background
         }
 
-
         if (text != null && price != null && imageDrawable != null) {
             // 다이얼로그에 정보 전달
             val dialog = optionDialog(
-                resources.getResourceEntryName(linearLayoutId),
-                text!!, price!!, imageDrawable!!
+                resources.getResourceEntryName(linearLayoutId), text!!, price!!, imageDrawable!!
             )
             dialog.show(activity?.supportFragmentManager!!, "CustomDialog")
         }
-    }
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            hotCof_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
