2ecdaa6ad2557ad9bcdf78493a820947142636e5
yjlee9909 <yjlee9909@naver.com>
2023-06-01 21:55:28 +0900

    ItemData.kt 코드 정렬

--- app/src/main/java/com/androidexlyj/lyj_kiosk/ItemData.kt
+++ app/src/main/java/com/androidexlyj/lyj_kiosk/ItemData.kt
@@ -1,6 +1,7 @@
 package com.androidexlyj.lyj_kiosk
 
-data class ItemData(val id: String, val name: String, val price: String, var count: Int,
-                    val selectedHotIceOption: String,val optShotName: String
-,val optShotPrice : Int) {
+data class ItemData(
+    val id: String, val name: String, val price: String, var count: Int,
+    val selectedHotIceOption: String, val optShotName: String, val optShotPrice: Int
+) {
 }
